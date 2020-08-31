edac1ae68652563b54141a802ca9d9b2eb127506
simo54 <sorzesimone1@gmail.com>
2020-08-31 17:57:28 +0200

    Bootstrap Framework installed, removed unnecessary files and codes

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Styles/App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>TEST</p>
+        Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
