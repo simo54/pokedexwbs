d4e7abee13a2d89351d5414c9097c595f863b8b2
Joschka Cesar <cesar@posteo.de>
2020-09-03 16:53:25 +0200

    Add Battlelog

--- src/Components/Fight.js
+++ src/Components/Fight.js
@@ -8,35 +8,36 @@ function Fight() {
   const fight = () => {
     // Naming for Readability
     const pokeOne = data[0].name;
-    const pokeTwo = data[1].name
+    const pokeTwo = data[1].name;
     let hpOne = data[0].stats[0].base_stat;
     let hpTwo = data[1].stats[0].base_stat;
     const attOne = data[0].stats[1].base_stat;
     const attTwo = data[1].stats[1].base_stat;
     const defOne = data[0].stats[2].base_stat;
     const defTwo = data[1].stats[2].base_stat;
+    const battleLog = document.querySelector(".battleLog");
 
     while (true) {
       if (hpOne <= 0) {
-      if (hpTwo <= 0) {
-        console.log("What a disaster! Both pokemon faint!");
-        break;
-      } else {
-      console.log(`${pokeOne} faints`);
-      break;
-      }
+        if (hpTwo <= 0) {
+            battleLog.innerHTML += `<p>What a disaster! Both pokemon faint!</p>`
+            break;
+        } else {
+            battleLog.innerHTML += `<p>${pokeOne} faints</p>`
+            break;
+        }
     } else if (hpTwo <= 0) {
-      console.log(`${pokeTwo} faints`);
-      break;
+        battleLog.innerHTML += `<p>${pokeTwo} faints</p>`
+        break;
     } else {
-      console.log("Round starts:");
-      console.log(`${pokeOne} attacks for ${attOne}`);
-      hpTwo -= Math.floor(attOne * (1 - defTwo / 100))
-      console.log(`${pokeTwo} defends with a value of ${defTwo} and takes ${Math.floor(attOne * (1 - defTwo / 100))} damage.`)
-      console.log(`${pokeTwo} attacks for ${attTwo}`);
-      hpOne -= Math.floor(attTwo * (1 - defOne / 100));
-      console.log(`${pokeOne} defends with a value of ${defOne} and takes ${Math.floor(attTwo * (1 - defOne / 100))} damage.`);
-      console.log(`remaining HP: ${pokeOne}: ${hpOne} - ${pokeTwo}: ${hpTwo}`)
+        battleLog.innerHTML += `<p>Round starts:</p>`
+        battleLog.innerHTML += `<p>${pokeOne} attacks for ${attOne}</p>`
+        hpTwo -= Math.floor(attOne * (1 - defTwo / 100))
+        battleLog.innerHTML += `<p>${pokeTwo} defends with a value of ${defTwo} and takes ${Math.floor(attOne * (1 - defTwo / 100))} damage.</p>`
+        battleLog.innerHTML += `<p>${pokeTwo} attacks for ${attTwo}</p>`
+        hpOne -= Math.floor(attTwo * (1 - defOne / 100));
+        battleLog.innerHTML += `<p>${pokeOne} defends with a value of ${defOne} and takes ${Math.floor(attTwo * (1 - defOne / 100))} damage.</p>`
+        battleLog.innerHTML += `<p>remaining HP: ${pokeOne}: ${hpOne} - ${pokeTwo}: ${hpTwo}</p>`
     }
   }
 
@@ -45,6 +46,7 @@ function Fight() {
   const changePokemon = (value, num) => {
     const copy = [...data];
     copy[num] = value;
+    copy[num].name = copy[num].name[0].toUpperCase() + copy[num].name.substring(1);
     setData(copy);
   }
 
@@ -53,6 +55,7 @@ function Fight() {
       <Fighter id={0} handleChange={(value) => changePokemon(value, 0)} poke={data} />
       <Fighter id={1} handleChange={(value) => changePokemon(value, 1)} poke={data}/>
       <button onClick={fight}>Fight!</button>
+      <div className="battleLog"></div>
    </div>
   );
 }
