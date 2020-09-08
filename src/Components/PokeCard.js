1852b836b93b047cd96b48f7b28bdfcf43d4010c
Joschka Cesar <cesar@posteo.de>
2020-09-08 18:40:29 +0200

    Add Buttons for access

--- src/Components/PokeCard.js
+++ src/Components/PokeCard.js
@@ -1,12 +1,20 @@
 import React, {useState} from 'react';
+import { Redirect } from 'react-router-dom';
 
 function PokeCard ({name, img}) {
+    const [redirect, setRedirect] = useState();
+
+    if (redirect) {
+        return <Redirect to={{pathname: redirect }} />
+    }
 
     return(
         <div className="card">
             <div className="card-body">
                 <img src={img} alt={name} width="100" />
                 <h5 className='card-title'>{name}</h5>
+                <button onClick={() => {setRedirect("/battle")}}>Fight</button>
+                <button>Details</button>
             </div>
         </div>
     )
