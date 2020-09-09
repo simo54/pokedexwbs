2214ce55ba2992ccb103a1be65df7a34988cba16
Joschka Cesar <cesar@posteo.de>
2020-09-09 16:31:28 +0200

    Add search bar

--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -24,6 +24,8 @@ function Pokedex() {
         setPokeNum(pokeNum + 20);
     }
 
+    const search= () => {}
+
     useEffect(() => {
         const pendingPromises = [];
         for (let i = pokeNum; i <= pokeNum + 19; i++) {
@@ -43,6 +45,23 @@ function Pokedex() {
 
     return (
         <div className="container">
+            {/* We hide the search bar during loading to prevent crashes in case the user immediately use the search bar */}
+            {pokelist && pokelist.length ? (
+        <>
+          <div className='container mt-3 d-flex justify-content-center'>
+            <img id='pikaHead' alt='pikachu' src='https://www.freepngimg.com/thumb/pokemon/37475-6-pikachu-transparent-image.png' width='50' height='50' />
+          </div>
+          <div className='container mb-5 mt-0 d-flex justify-content-center'>
+            {/* Search Input for filtering elements */}
+            <div>
+              <img alt='compass' src='https://cdn.icon-icons.com/icons2/851/PNG/512/Direction_icon-icons.com_67565.png' width='40' className='mr-2' />
+            </div>
+            <input type='text' onKeyUp={search} placeholder='Search here...' className='form-control w-25' aria-label='Sizing example input' aria-describedby='inputGroup-sizing-sm' id='inputSearch' />
+            <img id='badge' alt='badge' src='https://image.flaticon.com/icons/svg/189/189011.svg' width='40' height='40' className='ml-2' />
+          </div>
+        </>
+      ) : null}
+      {/* END of conditional operator */}
             <div className='row row-cols-4 m-auto mb-2'>
                 {error ? (
                     <div>
