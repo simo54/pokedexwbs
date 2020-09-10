705bf74642296c3369274c07d62e7d03bbdea4a2
simo54 <sorzesimone1@gmail.com>
2020-09-10 11:22:04 +0200

    created pokedex search functionality, implemented style, small fixes

--- src/Components/PokeCard.js
+++ src/Components/PokeCard.js
@@ -1,27 +1,28 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
-function PokeCard ({name, img, types, number, element}) {
-
-    return(
-        <div className="card">
-            <div className="card-body">
-                <img src={img} alt={name} width="100" />
-                <h5 className='card-title'>{number} {name}</h5>
-                <p>{types[0].type.name}</p>
-                {types[1] ? 
-                <p>{types[1].type.name}</p> :
-                null }
-                <Link to={{ pathname: `/battle/${number}`, state: {element}}}>
-                    <button>Fight</button>
-                </Link>
-                <Link to={`/details/${number}`}>
-                    <button>Details</button>
-                </Link>
-            </div>
-        </div>
-    )
-
+function PokeCard({ name, img, types, number, element }) {
+  return (
+    <div className='card'>
+      <div className='card-body'>
+        <img src={img} alt={name} width='100' />
+        <h5 className='card-title'>
+          #{number}
+          {""} {""}
+          {name}
+        </h5>
+        <p>{types[0].type.name}</p>
+        {types[1] ? <p>{types[1].type.name}</p> : null}
+        <Link to={{ pathname: `/battle/${number}`, state: { element } }}>
+          <Button className='mr-2'>Fight</Button>
+        </Link>
+        <Link to={`/details/${number}`}>
+          <Button className='ml-2'>Details</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default PokeCard;
--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -1,53 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import PokeCard from './PokeCard';
-import './Styles/Fight.css'
-
+import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import PokeCard from "./PokeCard";
+import Effectdex from "./EffectPokedex";
+import "./Styles/Fight.css";
 
 function Pokedex() {
-    const [pokelist, setPokelist] = useState();
-    const [error, setError] = useState(false);
-    const [pokeNum, setPokeNum] = useState(1);
+  const [pokelist, setPokelist] = useState();
+  const [error] = useState(false);
+  const [pokeNum, setPokeNum] = useState(1);
+  const [pokeSearch, setPokeSearch] = useState([]);
+  const [toggle, setToggle] = useState(false);
 
-    const loadMore = () => {
-        const pendingPromises = [];
-        for (let i = 1; i <= pokeNum + 19; i++) {
-            pendingPromises.push(
-            fetch(`http://pokeapi.co/api/v2/pokemon/${i}`)
-                .then((data) => data.json())
-                .then((data) => data)
-                .catch((e) => e)
-                );
-        }
-        Promise.all(pendingPromises).then((value) => {
-            setPokelist(value);
-            });
-        setPokeNum(pokeNum + 20);
+  const loadMore = () => {
+    const pendingPromises = [];
+    for (let i = 1; i <= pokeNum + 19; i++) {
+      pendingPromises.push(
+        fetch(`http://pokeapi.co/api/v2/pokemon/${i}`)
+          .then((data) => data.json())
+          .then((data) => data)
+          .catch((e) => e)
+      );
     }
+    Promise.all(pendingPromises).then((value) => {
+      setPokelist(value);
+    });
+    setPokeNum(pokeNum + 20);
+  };
 
-    const search= () => {}
+  function textSubmit(e) {
+    e.preventDefault();
+    const inputNumber = document.getElementById("inputSearch").value;
+    let firstPromise = new Promise((resolve, reject) => {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${inputNumber}`)
+        .then((value) => value.json())
+        .then((value) => resolve(value))
+        .catch(() => reject());
+    });
+    firstPromise
+      .then((value) => {
+        let name = value.name; // get the data from here and then display
+        let img = value.sprites.front_default;
+        let types = value.types;
+        let id = value.id;
+        setPokeSearch([{ name, img, types, id }]);
+        setToggle(true);
+      })
+      .catch(() => console.log("there was a problem"));
+  }
 
-    useEffect(() => {
-        const pendingPromises = [];
-        for (let i = pokeNum; i <= pokeNum + 19; i++) {
-            pendingPromises.push(
-            fetch(`http://pokeapi.co/api/v2/pokemon/${i}`)
-                .then((data) => data.json())
-                .then((data) => data)
-                .catch((e) => e)
-                );
-        }
-        Promise.all(pendingPromises).then((value) => {
-            setPokelist(value);
-            });
+  useEffect(() => {
+    const pendingPromises = [];
+    for (let i = pokeNum; i <= pokeNum + 19; i++) {
+      pendingPromises.push(
+        fetch(`http://pokeapi.co/api/v2/pokemon/${i}`)
+          .then((data) => data.json())
+          .then((data) => data)
+          .catch((e) => e)
+      );
+    }
+    Promise.all(pendingPromises).then((value) => {
+      setPokelist(value);
+    });
 
-        setPokeNum(pokeNum + 20);
-    }, []);
+    setPokeNum(pokeNum + 20);
+  }, []);
 
-    return (
-        <div className="container">
-            {/* We hide the search bar during loading to prevent crashes in case the user immediately use the search bar */}
-            {pokelist && pokelist.length ? (
-        <>
+  return (
+    <div className='container'>
+      {/* We hide the search bar during loading to prevent crashes in case the user immediately use the search bar */}
+      {pokelist && pokelist.length ? (
+        <form onSubmit={textSubmit}>
+          {toggle === true ? <Effectdex name={pokeSearch[0].name} img={pokeSearch[0].img} number={pokeSearch[0].id} types={pokeSearch[0].types} toggler={() => setToggle(false)} /> : null}
           <div className='container mt-3 d-flex justify-content-center'>
             <img id='pikaHead' alt='pikachu' src='https://www.freepngimg.com/thumb/pokemon/37475-6-pikachu-transparent-image.png' width='50' height='50' />
           </div>
@@ -56,29 +79,27 @@ function Pokedex() {
             <div>
               <img alt='compass' src='https://cdn.icon-icons.com/icons2/851/PNG/512/Direction_icon-icons.com_67565.png' width='40' className='mr-2' />
             </div>
-            <input type='text' onKeyUp={search} placeholder='Search here...' className='form-control w-25' aria-label='Sizing example input' aria-describedby='inputGroup-sizing-sm' id='inputSearch' />
+            <input min='1' max='807' type='text' placeholder='Search here and press enter!' className='form-control w-25' aria-label='Sizing example input' aria-describedby='inputGroup-sizing-sm' id='inputSearch' />
             <img id='badge' alt='badge' src='https://image.flaticon.com/icons/svg/189/189011.svg' width='40' height='40' className='ml-2' />
           </div>
-        </>
+        </form>
       ) : null}
       {/* END of conditional operator */}
-            <div className='row row-cols-4 m-auto mb-2'>
-                {error ? (
-                    <div>
-                        Something went wrong!
-                    </div>
-                ) : null}
-                {pokelist  ?
-                    (pokelist.map((element, index) => (
-                        <div className="col mb-4"key={index}>
-                            <PokeCard element={element} name={element.name} img={element.sprites.front_default} types={element.types} number={element.id}/>                        
-                        </div>
-                    )))
-                : null}
-            </div>
-            <button onClick={loadMore}>Load more</button>
-        </div>
-    )
+      <div className='row m-auto mb-2'>
+        {error ? <div>Something went wrong!</div> : null}
+        {pokelist
+          ? pokelist.map((element, index) => (
+              <div className='col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-6 mb-4' key={index}>
+                <PokeCard element={element} name={element.name} img={element.sprites.front_default} types={element.types} number={element.id} />
+              </div>
+            ))
+          : null}
+      </div>
+      <Button className='mt-4 mb-4' onClick={loadMore}>
+        Load more
+      </Button>
+    </div>
+  );
 }
 
 export default Pokedex;
